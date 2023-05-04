3b180560f08a9bc21d70d80f1c3703317a687670
Leonardo <leobusta@gmail.com>
2023-05-04 18:12:16 -0500

    clase  jueves 27 amyo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+import  dotenv from 'dotenv';
+import routes from './routes';
+import connect from './utils/connect';
+
+dotenv.config();
 
 const app: Express = express();
 
+const port: number = parseInt(process.env.PORT as string) || 5000;
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Express + typescript server');
 });
 
-app.listen(5000, () => console.log('Server running'));
+routes(app);
+
+connect();
+
+app.listen(port, () => console.log('Server running'));
